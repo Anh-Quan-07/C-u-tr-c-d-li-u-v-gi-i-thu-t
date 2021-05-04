fb2541ab7a7289da479a60a2366bd68d0a2bd2ce
Anh-Quan-07 <72288799+Anh-Quan-07@users.noreply.github.com>
2021-05-04 10:40:44 +0700

    Add files via upload

---
+++ sắp xếp chèn.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 10:39:46 2021
+
+@author: quann
+"""
+
+def insertion_sort(InputList):
+    for i in range(1, len(InputList)):
+        j = i - 1
+        nxt_element = InputList[i]
+        # Compare the current element with next one
+
+        while (InputList[j] > nxt_element) and (j >= 0):
+            InputList[j + 1] = InputList[j]
+            j = j - 1
+        InputList[j + 1] = nxt_element
+
+
+list = [19, 2, 31, 45, 30, 11, 121, 27]
+insertion_sort(list)
+print(list)
